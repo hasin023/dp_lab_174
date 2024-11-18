7e713f0604634a1e683c4a8eb4b18d28b3cae873
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-18 22:00:02 +0600

    Updated helpers

--- lab8/FindWeather/src/App.java
+++ lab8/FindWeather/src/App.java
@@ -1,18 +1,47 @@
+import utils.Helper;
 import models.WeatherData;
 import proxy.WeatherServiceProxy;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Helper helper = new Helper();
         WeatherServiceProxy weatherService = new WeatherServiceProxy();
+        WeatherData data;
 
-        try {
-            WeatherData data = weatherService.getWeatherData();
-            data.printWeatherDetails();
+        helper.clearScreen();
+        helper.printHeader();
+        while (true) {
+            helper.printMenu();
+            int option = helper.getOption();
 
-            WeatherData data2 = weatherService.getWeatherData();
-            data2.printWeatherDetails();
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
+            switch (option) {
+                case 1:
+                    helper.printWeatherMenu();
+                    int weatherOption = helper.getOption();
+
+                    switch (weatherOption) {
+                        case 1:
+                            data = weatherService.getWeatherData();
+                            helper.displayData(data);
+                            break;
+                        case 2:
+                            data = weatherService.getWeatherData();
+                            helper.displayData(data);
+                            break;
+                        default:
+                            System.out.println("Invalid option");
+                    }
+                    break;
+                case 2:
+                    weatherService.resetCache();
+                    helper.printSuccess("Cache reset successfully");
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
         }
     }
 }
--- lab8/FindWeather/src/models/WeatherData.java
+++ lab8/FindWeather/src/models/WeatherData.java
@@ -15,8 +15,20 @@ public class WeatherData {
         this.timestamp = System.currentTimeMillis();
     }
 
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
     public String getLocation() {
         return location;
+    }
+
+    public String getSource() {
+        return source;
     }
 
     public long getTimestamp() {
@@ -26,19 +38,4 @@ public class WeatherData {
     public void updateSource(String value) {
         this.source = value;
     }
-
-    public void printWeatherDetails() {
-        System.out.println("\n");
-        System.out.println("Requested data for {{" + location + "}} -->>");
-        System.out.println("-----------------------------------------------------------------");
-        System.out.println("Temperature: " + temperature + "°C");
-        System.out.println("Conditions: " + conditions);
-        System.out.println("Source: " + source);
-        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String readableDate = sdf.format(new java.util.Date(timestamp));
-        System.out.println("Timestamp: " + readableDate);
-        System.out.println("-----------------------------------------------------------------");
-        System.out.println("\n");
-    }
-
 }
