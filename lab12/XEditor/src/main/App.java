041018bb878d71e6ef765a49fc686656efe2188d
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2025-03-14 12:15:46 +0600

    Added issue #4

--- lab12/XEditor/src/main/App.java
+++ lab12/XEditor/src/main/App.java
@@ -1,25 +1,48 @@
 package main;
 
 import main.core.TextEditor;
+import main.features.History;
 import main.memento.EditorMemento;
 
 public class App {
     public static void main(String[] args) throws Exception {
         TextEditor editor = new TextEditor();
+        History history = new History(editor);
 
+        // 1st state
         editor.SetContent("Goodbye World 🔥🔥");
         editor.SetCursorPosition(5);
         editor.AddSelection("World");
         editor.DisplayState();
+        EditorMemento state1 = history.Backup();
+        System.out.println("State 1 saved to history");
 
-        EditorMemento memento = editor.CreateMemento();
-
-        editor.SetContent("Final Farewell World 🔥🔥");
+        // 2nd state
+        editor.SetContent("Farewell World 🔥🔥");
         editor.SetCursorPosition(16);
         editor.ClearSelections();
         editor.DisplayState();
+        EditorMemento state2 = history.Backup();
+        System.out.println("State 2 saved to history");
 
-        editor.RestoreFromMemento(memento);
+        // 3rd state
+        editor.SetContent("Final Farewell World 🔥🔥");
+        editor.SetCursorPosition(22);
+        editor.ClearSelections();
+        editor.DisplayState();
+        EditorMemento state3 = history.Backup();
+        System.out.println("State 3 saved to history");
+
+        // System.out.println("Reverting to previous state...");
+        // history.Undo();
+        // editor.DisplayState();
+
+        System.out.println("Reverting to state 1");
+        editor.RestoreFromMemento(state1);
+        editor.DisplayState();
+
+        System.out.println("Reverting to state 3");
+        editor.RestoreFromMemento(state3);
         editor.DisplayState();
     }
 }
--- lab12/XEditor/src/main/features/History.java
+++ lab12/XEditor/src/main/features/History.java
@@ -1,21 +1,31 @@
 package main.features;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 import main.core.TextEditor;
 import main.memento.EditorMemento;
 
 public class History {
     private TextEditor editor;
-    private final List<EditorMemento> mementos;
+    private final Stack<EditorMemento> mementos;
 
     public History(TextEditor editor) {
         this.editor = editor;
-        this.mementos = new ArrayList<>();
+        this.mementos = new Stack<>();
     }
 
-    public void Backup() {
-        this.mementos.add(this.editor.CreateMemento());
+    public EditorMemento Backup() {
+        EditorMemento newMemento = this.editor.CreateMemento();
+        this.mementos.push(newMemento);
+        return newMemento;
+    }
+
+    public void Undo() {
+        if (this.mementos.isEmpty()) {
+            return;
+        }
+
+        EditorMemento memento = this.mementos.pop();
+        this.editor.RestoreFromMemento(memento);
     }
 }
